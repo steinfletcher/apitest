c06cb30d5fea8122c57cbbf81f40b1b783144ede
steinfletcher <code@stein.wtf>
2025-03-05 09:39:46 +0000

    Fix formatting

--- filesystem.go
+++ filesystem.go
@@ -5,10 +5,11 @@ import (
 	"os"
 )
 
-//An implementation of fs.FS that wraps your OS's filesystem
+// An implementation of fs.FS that wraps your OS's filesystem
 type OSFS struct {
 }
-//Calls os.Open
+
+// Calls os.Open
 func (OSFS) Open(name string) (file fs.File, err error) {
 	return os.Open(name)
 }
